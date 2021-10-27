4a16a3dee24a70ea2b50b9a0e96271c0ff127129
JuanDLGZ <42816824+JuanDLGZ@users.noreply.github.com>
2021-10-27 17:25:48 -0500

    Add Vehicle Types

---
+++ Vehicles.API/Controllers/VehicleTypesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public VehicleTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VehicleTypes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleType);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Description")] VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicleType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Description")] VehicleType vehicleType)
+        {
+            if (id != vehicleType.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicleType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleTypeExists(vehicleType.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicleType);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicleType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vehicleType = await _context.VehicleTypes.FindAsync(id);
+            _context.VehicleTypes.Remove(vehicleType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleTypeExists(int id)
+        {
+            return _context.VehicleTypes.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        [Key]
+        public int ID { set; get; }
+
+        [Display(Name ="Type")]
+        [MaxLength(50,ErrorMessage ="This field {0} cannot over {1} digits.")]
+        [Required(ErrorMessage ="This field {0} is requiered.")]
+        public string Description { set; get; }
+    }
+}
